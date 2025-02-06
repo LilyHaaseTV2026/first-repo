28191ed227f3edb8c2a2adfca98ae26851a3dd6f
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-06 13:25:33 -0700

    boxy

--- starsandwords.py
+++ starsandwords.py
@@ -1,1 +1,10 @@
+string = "hello world in a frame"
 
+def starsIntoWords(string):
+    x = string.split()
+    print(x)
+    print('*********')
+    for words in x:
+        print('* ' + words + ' *')
+    print('*********')
+starsIntoWords(string)
